8b9217f3121a5490ba6f704e8ce7beb281024c89
Juliaz84 <143300965+Juliaz84@users.noreply.github.com>
2025-11-12 21:06:49 -0300

    Add files via upload

---
+++ aula_13/aula13.py
@@ -0,0 +1,22 @@
+import modulo_aula13 as md
+
+
+def atv():
+
+    print(f'Aleatorio {md.num()}')
+
+    print(f'aleatorio: {md.num2()}')
+
+    print(f'Aleatorio {md.num3()}')
+    
+    print(f'{md.num4()}')
+
+    print(f'{md.num5()}')
+
+    print(f'{md.num6()}')
+
+    print(f'{md.num7()}')
+
+    
+
+atv()
